e0454455af5cfa94891e8d45fab33452f4b038ad
keyboardkardio <keyboardkardio@gmail.com>
2023-02-07 18:17:42 -0800

    add global port for host

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,6 @@
 import { server } from './server';
 
-const SERVER_PORT = process.env.SERVER_PORT;
+const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
 server.listen(SERVER_PORT, () => {
     console.log(`Listening on port ${SERVER_PORT}`);
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 
 import { userRouter } from './user/user.router';
 import { exerciseRouter } from './exercise/exercise.router';
@@ -8,6 +9,7 @@ import { workoutRouter } from './workout/workouts.router';
 export const server = express();
 server.use(express.json());
 server.use(cors());
+server.use(helmet());
 
 server.use('/api/users', userRouter);
 server.use('/api/exercises', exerciseRouter);
