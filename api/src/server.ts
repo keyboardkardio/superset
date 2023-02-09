a358538a8e609ac6b2e747cc9dc13216d28df53d
keyboardkardio <keyboardkardio@gmail.com>
2023-02-08 17:47:32 -0800

    allow unused parameters

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+
 import { userRouter } from './user/user.router';
 import { exerciseRouter } from './exercise/exercise.router';
 import { workoutRouter } from './workout/workouts.router';
+
+import { errorHandler } from './shared/middleware/error.middleware';
+import { notFoundHandler } from './shared/middleware/not-found.middleware';
 
 export const server = express();
 server.use(express.json());
@@ -18,3 +22,6 @@ server.use(helmet());
 server.use('/api/users', userRouter);
 server.use('/api/exercises', exerciseRouter);
 server.use('/api/workouts', workoutRouter);
+
+server.use(errorHandler);
+server.use(notFoundHandler);
