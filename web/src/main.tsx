1cd28d6fb62f93524283b966a8f50d4073255782
keyboardkardio <keyboardkardio@gmail.com>
2023-02-09 16:13:15 -0800

    remove styled-components

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { Box, ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { router } from '@/router';
-import { ContainerApp } from '@/components';
-import barbell from '@/assets/images/barbell.jpg'
+import barbell from '@/assets/images/barbell.jpg';
 
 const fonts = {
     default: {
@@ -18,9 +17,22 @@ const theme = extendTheme({ fonts });
 createRoot(document.getElementById('root') as HTMLElement).render(
     <StrictMode>
         <ChakraProvider theme={theme}>
-            <ContainerApp backgroundImage={barbell}>
+            <Box
+                backdropBlur={'3xl'}
+                backdropFilter={'auto'}
+                bgColor={'gray.700'}
+                bgImg={`linear-gradient(rgba(22, 22, 22, 0.702), 
+                        rgba(0, 0, 0, 0.946)), url(${barbell})`}
+                bgRepeat={'no-repeat'}
+                bgSize={'cover'}
+                display={'flex'}
+                flexDirection={'column'}
+                justifyContent={'space-between'}
+                minH={'100vh'}
+                padding={'2.8rem 1.4rem'}
+            >
                 <RouterProvider router={router} />
-            </ContainerApp>
+            </Box>
         </ChakraProvider>
     </StrictMode>,
 );
