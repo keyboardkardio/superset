9603c6ebeab1f14e4bde1148099e205b1385a23a
keyboardkardio <keyboardkardio@gmail.com>
2023-02-24 04:23:11 -0800

    add basic validation

--- api/src/user/user.router.ts
+++ api/src/user/user.router.ts
@@ -5,13 +5,21 @@ export const userRouter = Router();
 
 userRouter.post('/register', async (request: Request, response: Response) => {
     const { username, password, passwordConfirmation } = request.body;
+    if (!username) {
+        response.status(400).json({ error: 'Username is required.' });
+    }
+    if (!password) {
+        response.status(400).json({ error: 'Password is required.' });
+    }
+    if (!passwordConfirmation) {
+        response.status(400).json({ error: 'Please confirm your password.' });
+    }
     if (password !== passwordConfirmation) {
-        throw new Error('Passwords do not match.');
+        response.status(400).json({ error: 'Passwords do not match.' });
     }
 
     try {
         const user = await authService.createNewUser(username, password);
-
         response.status(201).json(user);
     } catch (error: any) {
         response.status(500).json({ error: error.message });
@@ -20,8 +28,16 @@ userRouter.post('/register', async (request: Request, response: Response) => {
 
 userRouter.post('/login', async (request: Request, response: Response) => {
     const { username, password } = request.body;
+    if (!username) {
+        response.status(400).json({ error: 'Username is required.' });
+    }
+    if (!password) {
+        response.status(400).json({ error: 'Password is required.' });
+    }
+
     try {
-        response.status(200).json(await authService.loginUser(username, password));
+        const token = await authService.loginUser(username, password);
+        response.status(200).json({ token });
     } catch (error: any) {
         response.status(500).json({ error: error.message });
     }
